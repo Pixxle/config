fcf6c898a1885bea3ea47ed18d296b6cfa7e2269
Dennis Vinterfjärd <dennisvinterfjard@Denniss-Mac-mini.local>
2024-02-15 20:31:33 +0100

    Adding lazygit to terminal aliases

--- shell/functions.d/alias.sh
+++ shell/functions.d/alias.sh
@@ -2,25 +2,22 @@ alias grep='rg'
 alias cat='bat'
 alias find='fd'
 alias ls='eza'
-alias of='fd --type f --hidden --exclude .git --exclude node_modules | fzf-tmux -p | xargs nvim'
-alias cl='cd $(find . -t directory --ignore-file $HOME/.config/shell/dirignore | fzf)'
+
 alias vim='nvim'
 alias v='nvim'
-alias pretty='prettier --write "**/*.{ts, tsx, js, jsx}"'
-alias gitls='git log --pretty=format:"%C(green)%h\ %C(yellow)[%ad]%Cred%d\ %Creset%s%Cblue\ [%cn]" --decorate --date=relative'
-alias gitll='git log --pretty=format:"%C(yellow)%h%Cred%d\ %Creset%s%Cblue\ [%cn]" --decorate --numstat'
 alias config='nvim ~/.config/'
-alias cdconf='cd ~/.config/'
+
 alias k='kubectl'
 alias kc='kubectl ctx'
 alias kn='kubectl ns'
 alias kreboot='k rollout restart deploy'
+
+alias of='fd --type f --hidden --exclude .git --exclude node_modules | fzf-tmux -p | xargs nvim'
+alias cl='cd $(find . -t directory --ignore-file $HOME/.config/shell/dirignore | fzf)'
+
+alias gitll='git log --pretty=format:"%C(yellow)%h%Cred%d\ %Creset%s%Cblue\ [%cn]" --decorate --numstat'
+alias gitls='git log --pretty=format:"%C(green)%h\ %C(yellow)[%ad]%Cred%d\ %Creset%s%Cblue\ [%cn]" --decorate --date=relative'
 alias gitclean='git branch --merged | /usr/bin/grep -v \* | xargs git branch -D'
+alias lg='lazygit'
 
-function klog() {
-  k logs --follow $1
-}
-
-function kgrep() {
-  k get $1 | grep $2
-}
+alias pretty='prettier --write "**/*.{ts, tsx, js, jsx}"'
