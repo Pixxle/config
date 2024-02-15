42fe833d28de4a4036b74df204bb37b302442466
Dennis Vinterfjärd <dennisvinterfjard@Denniss-Mac-mini.local>
2024-02-15 18:21:05 +0100

    Changing how i call ts plugin to be more uniform with rest

--- nvim/lua/core/plugin_config/typescript_config.lua
+++ nvim/lua/core/plugin_config/typescript_config.lua
@@ -1,8 +1,4 @@
-require("typescript").setup({})
-local typescript = require 'typescript'
-
-typescript.setup({
-    disable_commands = false, -- prevent the plugin from creating Vim commands
-    debug = false, -- enable debug logging for commands
+require("typescript").setup({
+    disable_commands = false,
+    debug = false,
 })
-
