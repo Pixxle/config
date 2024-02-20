626352dca91b1396440b94f0b488b816dd2eb62c
Dennis Vinterfjärd <dennis@vinterfjard.com>
2024-02-20 20:14:30 +0100

    migrating from nvim-tree to neo-tree

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -39,7 +39,16 @@ require("lazy").setup({
     { 'nvim-telescope/telescope.nvim' },
 
     --nvim-tree
-    { 'kyazdani42/nvim-tree.lua' },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        }
+    },
    
     -- tresitter
     { 'nvim-treesitter/nvim-treesitter-textobjects' },
