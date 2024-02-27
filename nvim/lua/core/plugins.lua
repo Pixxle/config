25d588e2874da12c5bd4cb6aef0b08c45a3205da
Dennis Vinterfjärd <dennisvinterfjard@Denniss-Mac-mini.local>
2024-02-27 20:52:37 +0100

    Adding Surround capabilities

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -1,68 +1,76 @@
 require("lazy").setup({
-    -- themes
-    { 'nyoom-engineering/oxocarbon.nvim' },
+	-- themes
+	{ "nyoom-engineering/oxocarbon.nvim" },
 
-    -- statusline
-    { 
-        'hoob3rt/lualine.nvim',
-        dependencies = {
-            "nvim-tree/nvim-web-devicons"
-        }
-    },
+	-- statusline
+	{
+		"hoob3rt/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
 
-    -- Copilot
-    { 'github/copilot.vim' },
+	-- Copilot
+	{ "github/copilot.vim" },
 
-    -- Configuration and abstraction layer
-    { 'nvim-lua/plenary.nvim' },
+	-- Configuration and abstraction layer
+	{ "nvim-lua/plenary.nvim" },
 
-    -- LSP Package manager
-    { 'williamboman/mason.nvim' },
-    { 'williamboman/mason-lspconfig.nvim' },
- 
-    -- LSP:
-    { 'neovim/nvim-lspconfig' },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'VonHeikemen/lsp-zero.nvim' },
-    { 'stevearc/conform.nvim' },
+	-- LSP Package manager
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
 
-    -- tmux navigation
-    { 'christoomey/vim-tmux-navigator' },
+	-- LSP:
+	{ "neovim/nvim-lspconfig" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "VonHeikemen/lsp-zero.nvim" },
+	{ "stevearc/conform.nvim" },
 
-    -- Split 
-    { 'declancm/maximize.nvim' }, 
- 
-    -- completion stuff:
-    { 'hrsh7th/cmp-buffer', },
-    { 'hrsh7th/cmp-path' },
-    { 'hrsh7th/cmp-cmdline' },
-    { 'hrsh7th/nvim-cmp' },
-    { 'saadparwaiz1/cmp_luasnip' },
+	-- tmux navigation
+	{ "christoomey/vim-tmux-navigator" },
 
-    -- Hop:
-    { 'phaazon/hop.nvim' },    
-   
-    -- telescope
-    { 'nvim-telescope/telescope.nvim' },
+	-- Split
+	{ "declancm/maximize.nvim" },
 
-    --neo-tree
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-        }
-    },
-   
-    -- tresitter
-    { 'nvim-treesitter/nvim-treesitter-textobjects' },
-    { 'nvim-treesitter/nvim-treesitter' },
+	-- completion stuff:
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{ "hrsh7th/cmp-cmdline" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "saadparwaiz1/cmp_luasnip" },
 
-    -- github
-    { 'kdheepak/lazygit.nvim' },
-    { 'f-person/git-blame.nvim' },
+	-- Hop:
+	{ "phaazon/hop.nvim" },
 
+	-- telescope
+	{ "nvim-telescope/telescope.nvim" },
+
+	-- Quick surround
+	{
+		"kylechui/nvim-surround",
+		version = "*",
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({})
+		end,
+	},
+	--neo-tree
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+	},
+
+	-- tresitter
+	{ "nvim-treesitter/nvim-treesitter-textobjects" },
+	{ "nvim-treesitter/nvim-treesitter" },
+
+	-- github
+	{ "kdheepak/lazygit.nvim" },
+	{ "f-person/git-blame.nvim" },
 })
