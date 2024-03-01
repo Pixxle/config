853630284aaec4521cfd78d83ff2bdd695c9be73
Dennis Vinterfjärd <dennisvinterfjard@Denniss-Mac-mini.local>
2024-03-01 16:04:14 +0100

    Fixing neo-tree to open on right and display the current file when opening

--- nvim/lua/core/plugin_config/neo-tree_config.lua
+++ nvim/lua/core/plugin_config/neo-tree_config.lua
@@ -267,5 +267,24 @@ require("neo-tree").setup({
 	},
 })
 
-vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
-vim.keymap.set("n", "<leader>fe", "<cmd>:Neotree toggle<CR>") -- open/close
+vim.keymap.set("n", "<leader>fe", function()
+	local reveal_file = vim.fn.expand("%:p")
+	if reveal_file == "" then
+		reveal_file = vim.fn.getcwd()
+	else
+		local f = io.open(reveal_file, "r")
+		if f then
+			f.close(f)
+		else
+			reveal_file = vim.fn.getcwd()
+		end
+	end
+	require("neo-tree.command").execute({
+		action = "focus", -- OPTIONAL, this is the default value
+		toggle = true, -- OPTIONAL, this is the default value
+		source = "filesystem", -- OPTIONAL, this is the default value
+		position = "right", -- OPTIONAL, this is the default value
+		reveal_file = reveal_file, -- path to file or folder to reveal
+		reveal_force_cwd = true, -- change cwd without asking if needed
+	})
+end)
