9500aa8e63f0a3650102793b2af6777637997612
Dennis Vinterfjärd <dennis@vinterfjard.com>
2024-03-01 18:59:44 +0100

    Removing some keybindings and binding them to when the lsp attaches instead

--- nvim/lua/core/plugin_config/lsp_zero.lua
+++ nvim/lua/core/plugin_config/lsp_zero.lua
@@ -9,8 +9,3 @@ lsp_zero.on_attach(function(client, bufnr)
 	vim.keymap.set("n", "<leader>h", "<cmd>lua vim.lsp.buf.hover()<cr>", { buffer = bufnr })
 end)
 lsp_zero.setup()
-
---vim.api.nvim_set_keymap("n", "<leader>rn", [[<cmd>lua vim.lsp.buf.rename()<CR>]], { silent = true })
---vim.api.nvim_set_keymap("n", "<leader>ca", [[<cmd>lua vim.lsp.buf.code_action()<CR>]], { silent = true })
---vim.api.nvim_set_keymap("n", "<leader>h", [[<cmd>lua vim.lsp.buf.hover()<CR>]], { silent = true })
---vim.api.nvim_set_keymap("n", "<leader>gi", [[<cmd>lua vim.lsp.buf.implementation()<CR>]], { silent = true })
