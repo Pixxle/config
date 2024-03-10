4542842f22f065da8f6d78737c83ed1371415a69
Dennis Vinterfjärd <dennisvinterfjard@Denniss-Mac-mini.local>
2024-03-10 23:55:37 +0100

    Adding function to get local book_marks for each project

--- nvim/lua/core/plugin_config/bookmarks_config.lua
+++ nvim/lua/core/plugin_config/bookmarks_config.lua
@@ -1,6 +1,12 @@
+function get_bookmark_file()
+	root = vim.fn.expand("$HOME/.bookmarks/")
+	cwd_path = vim.fn.getcwd():gsub("%/", "_")
+	return root .. "/" .. cwd_path .. ".bookmark"
+end
+
 require("bookmarks").setup({
 	-- sign_priority = 8,  --set bookmark sign priority to cover other sign
-	save_file = vim.fn.expand("$HOME/.bookmarks"), -- bookmarks save file path
+	save_file = get_bookmark_file(), -- set bookmark file path
 	keywords = {
 		["@t"] = "☑️ ", -- mark annotation startswith @t ,signs this icon as `Todo`
 		["@w"] = "⚠️ ", -- mark annotation startswith @w ,signs this icon as `Warn`
