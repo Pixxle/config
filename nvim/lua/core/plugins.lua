7fb830ebdcd678180b7bcc57ee17e8eae679fbb7
Dennis Vinterfjärd <dennis@vinterfjard.com>
2024-08-28 09:55:46 +0200

    Removing avante, not that impressed by it

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -28,42 +28,6 @@ require("lazy").setup({
 			require("supermaven-nvim").setup({})
 		end,
 	},
-	---- Avente - Code asking etc
-	{
-		"yetone/avante.nvim",
-		event = "VeryLazy",
-		build = "make", -- This is Optional, only if you want to use tiktoken_core to calculate tokens count
-		opts = {
-			-- add any opts here
-			provider = "claude",
-			mappings = {
-				ask = "<leader>.",
-				edit = "<leader>,",
-				refresh = "<leader>,,",
-			},
-		},
-		dependencies = {
-			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-			"stevearc/dressing.nvim",
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-			--- The below is optional, make sure to setup it properly if you have lazy=true
-			{
-				"MeanderingProgrammer/render-markdown.nvim",
-				opts = {
-					file_types = { "markdown", "Avante" },
-				},
-				ft = { "markdown", "Avante" },
-			},
-			--- For image pasting support
-			{
-				"HakonHarnes/img-clip.nvim",
-				event = "VeryLazy",
-				opts = {},
-			},
-		},
-	},
-
 	-- Configuration and abstraction layer
 	{ "nvim-lua/plenary.nvim" },
 
