888cbc13b9eab41d49ff796b9d8505ae200cffdb
Dennis Vinterfjärd <dennis@vinterfjard.com>
2024-12-11 13:19:24 +0100

    Switch neotree to left again

--- nvim/lua/core/plugin_config/neo-tree_config.lua
+++ nvim/lua/core/plugin_config/neo-tree_config.lua
@@ -283,7 +283,7 @@ vim.keymap.set("n", "<leader>-", function()
 		action = "focus", -- OPTIONAL, this is the default value
 		toggle = true, -- OPTIONAL, this is the default value
 		source = "filesystem", -- OPTIONAL, this is the default value
-		position = "right", -- OPTIONAL, this is the default value
+		position = "left", -- OPTIONAL, this is the default value
 		reveal_file = reveal_file, -- path to file or folder to reveal
 		reveal_force_cwd = true, -- change cwd without asking if needed
 	})
