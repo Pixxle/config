f04352879bdbec2be0c8e013f10324d4f0233725
Dennis Vinterfjärd <dennis@vinterfjard.com>
2025-01-14 16:21:45 +0100

    Adding some git functions

---
+++ shell/functions.d/git.sh
@@ -0,0 +1,18 @@
+function gdd() {
+    local selected=$(git branch | fzf)
+    selected=${selected// /}
+    if [ -n "$selected" ]; then
+        git branch -D "$selected"
+    else
+        echo "No branch selected, aborting..."
+    fi
+}
+
+# remove alias if it exists
+if alias gd >/dev/null 2>&1; then
+    unalias gd
+fi
+function gd() {
+  preview="git diff $@ --color=always -- {-1}"
+  git diff $@ --name-only | fzf -m --ansi --preview $preview
+}
