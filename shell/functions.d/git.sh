6ae8b3650fc74437a96d2a75322de2b17b7bf879
Dennis Vinterfjärd <dennis@vinterfjard.com>
2025-01-14 16:27:05 +0100

    Adding functionality to switch branch

--- shell/functions.d/git.sh
+++ shell/functions.d/git.sh
@@ -16,3 +16,17 @@ function gd() {
   preview="git diff $@ --color=always -- {-1}"
   git diff $@ --name-only | fzf -m --ansi --preview $preview
 }
+
+if alias gs >/dev/null 2>&1; then
+    unalias gs
+fi
+function gs() {
+    local selected=$(git branch | fzf)
+    selected=${selected// /}
+    if [ -n "$selected" ]; then
+        git checkout "$selected"
+        git pull
+    else
+        echo "No branch selected, aborting..."
+    fi
+}
