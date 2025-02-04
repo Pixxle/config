4549a976567146e5989925daa25c53a34d9e093c
Dennis Vinterfjärd <dennis@vinterfjard.com>
2025-02-04 08:44:17 +0100

    Fixing unknown reference for mason

--- nvim/lua/core/plugin_config/mason.lua
+++ nvim/lua/core/plugin_config/mason.lua
@@ -2,6 +2,7 @@ require("mason").setup()
 
 local lspconfig = require("lspconfig")
 local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+local vim = vim
 
 require("mason-lspconfig").setup({
 	ensure_installed = {
