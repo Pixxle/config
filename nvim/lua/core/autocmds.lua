ac10801dad29b9ba319185e64a20e5044ccd12a0
Dennis Vinterfjärd <dennis@vinterfjard.com>
2025-07-06 00:57:05 +0200

    Update and removing things

--- nvim/lua/core/autocmds.lua
+++
@@ -1,5 +0,0 @@
---
-local cmd = vim.cmd
---
--------------------- COMMANDS ------------------------------
-cmd("au TextYankPost * lua vim.highlight.on_yank {on_visual = false}") -- disabled in visual mode
--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -22,8 +22,3 @@ vim.keymap.set("n", ";", ":")
 vim.keymap.set("n", "<leader>w", "<cmd>:w<CR>")
 vim.keymap.set("n", "<leader>q", "<cmd>:q<CR>")
 vim.keymap.set("n", "<leader>Q", "<cmd>:q!<CR>")
-
-vim.keymap.set("n", "<leader>cc", "<cmd>CopilotChatToggle<CR>")
-
----g.copilot_no_tab_map = true
----g.copilot_assume_mapped = true
