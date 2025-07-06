1edae81ebc02012faec55d25f86242bd34d068ef
Dennis Vinterfjärd <dennis@vinterfjard.com>
2025-07-06 02:37:46 +0200

    Adding fuzzy history search

--- shell/functions.d/fuzzy.sh
+++ shell/functions.d/fuzzy.sh
@@ -5,4 +5,17 @@ alias vl='fd --type f --hidden --exclude .git --exclude node_modules | fzf-tmux 
 # Fuzzy cd finder
 alias cl='cd $(find . -t directory --ignore-file $HOME/.config/shell/dirignore | fzf)'
 
+# Fuzzy history search
+fzf_history() {
+  local selected
+  selected=$(fc -l 1 | awk '{$1=""; print substr($0,2)}' | sort | uniq | fzf --tac --no-sort)
+  if [[ -n "$selected" ]]; then
+    BUFFER="$selected"
+    CURSOR=$#BUFFER
+    zle redisplay
+  fi
+}
+zle -N fzf_history
+bindkey '^R' fzf_history
+
 
